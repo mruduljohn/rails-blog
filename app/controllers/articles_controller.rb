7595cc2c5bac722bc38dfb16a4bd204e590dd6ba
Mrudul John <mrudulmathews@gmail.com>
2024-05-03 02:37:04 +0530

    9.2 Commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
